148d6ebaf1a1f56d2eb405ad2954abb99b3d7364
alexamazzing <alexamazzing@gmail.com>
2022-03-27 12:54:33 +0500

    *

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,22 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+
         System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+
+        System.out.println(bean == bean1);
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(cat.getMessage());
+        System.out.println(cat1.getMessage());
+
+        System.out.println(cat == cat1);
+
+
     }
 }
